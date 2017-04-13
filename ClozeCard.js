1170a3a6117d4d72f4d2883cc2cdf633310535a3
mkearney@google.com <mkearney@google.com>
2017-04-13 12:57:12 -0700

    clean up before extra credit

--- ClozeCard.js
+++ ClozeCard.js
@@ -6,27 +6,29 @@ var BasicCard = require("./BasicCard.js");
 var ClozeCard = function(text, cloze) {
 	this.fullText = text;
 	this.cloze = cloze;
+	this.partial = function() {
+		if (this.fullText.includes(this.cloze)) {
+			return this.fullText.replace(this.cloze, '...');
+		} else {
+			var brokenClozeMessage = "Oops! The full text: '" + this.fullText + "' doesn't contain the cloze: '" + this.cloze + "'.";
+			return brokenClozeMessage;
+		}
+	};
 };
-
-// Returns partial text
-ClozeCard.prototype.returnPartialText = function()  {
- 	
- 	if (this.fullText.includes(this.cloze)) {
- 		var partial = this.fullText.replace(this.cloze, '...');
- 		console.log("Partial: " + partial);
- 		return this.partial;
- 	} else {
- 		console.log("Oops! Looks like " + this.fullText + " does not contain " + this.cloze);
- 	}
-
-}
 
 var firstPresident = new BasicCard("Who was the first president of the US?", "George Washington");
 
 console.log(firstPresident.front);
-
 console.log(firstPresident.back);
 
 var firstPresidentCloze = new ClozeCard("George Washington was the first president of the US.", "George Washington");
 
-firstPresidentCloze.returnPartialText();
+console.log(firstPresidentCloze.fullText);
+console.log(firstPresidentCloze.cloze);
+console.log(firstPresidentCloze.partial());
+
+var typoPresidentCloze = new ClozeCard("Donald Trump is the current president of the US.", "Donald Frump");
+
+console.log(typoPresidentCloze.fullText);
+console.log(typoPresidentCloze.cloze);
+console.log(typoPresidentCloze.partial());
